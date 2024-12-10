0a15d0ec658d5561c3f32c8e7516013f995fe03b
Brayam <102553162+Brazamm@users.noreply.github.com>
2024-12-09 22:46:53 -0500

    Create IhistorialJpa.java

---
+++ gestask/src/main/java/co/edu/uniempresarial/repository/IhistorialJpa.java
@@ -0,0 +1,30 @@
+package co.edu.uniempresarial.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import co.edu.uniempresarial.model.historial;
+
+
+public interface IhistorialJpa extends JpaRepository<historial,Integer> {
+
+}
+package co.edu.uniempresarial.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import co.edu.uniempresarial.model.historial;
+
+
+public interface IhistorialJpa extends JpaRepository<historial,Integer> {
+
+}
+package co.edu.uniempresarial.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import co.edu.uniempresarial.model.historial;
+
+
+public interface IhistorialJpa extends JpaRepository<historial,Integer> {
+
+}
