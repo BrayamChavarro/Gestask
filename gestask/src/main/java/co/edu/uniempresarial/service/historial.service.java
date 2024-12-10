e3e501f279b72dfef66a00f3b31172ec647ebe0a
Brayam <102553162+Brazamm@users.noreply.github.com>
2024-12-09 22:47:47 -0500

    Create historial.service.java

---
+++ gestask/src/main/java/co/edu/uniempresarial/service/historial.service.java
@@ -0,0 +1,44 @@
+package co.edu.uniempresarial.service;
+import java.util.List;
+    
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import co.edu.uniempresarial.model.historial;
+import co.edu.uniempresarial.repository.Ihistorial;
+
+@Service
+    public class historialService implements IhistorialService {
+        
+        //inyeccion de dependencias - repository
+        @Autowired
+        Ihistorial dao;
+    
+        @Override
+        public List<historial> agregarhistorial(historial historial) {
+            return dao.addhistorial(historial);
+        }
+    
+        @Override
+        public historial actualizarhistorial(historial historial) {
+            //busqueda del id a ver si existe
+            return dao.upphistorial(historial);
+        }
+    
+        @Override
+        public List<historial> todashistorial() {
+            return dao.getAllhistorial();
+        }
+    
+        @Override
+        public historial buscarIdhistorial(int id) {
+            return dao.getIdhistorial(id);
+        }
+
+        @Override
+        public boolean bajahistorial(int id) {
+            // TODO Auto-generated method stub
+            return false;
+        }
+    
+    }
