a74171883ce07b289cda768e2e42da150f978686
Brayam <102553162+Brazamm@users.noreply.github.com>
2024-12-09 21:24:34 -0500

    Create comentariosService.java

---
+++ gestask/src/main/java/co/edu/uniempresarial/service/comentariosService.java
@@ -0,0 +1,44 @@
+package co.edu.uniempresarial.service;
+import java.util.List;
+    
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import co.edu.uniempresarial.model.comentarios;
+import co.edu.uniempresarial.repository.Icomentarios;
+
+@Service
+    public class comentariosService implements IcomentariosService {
+        
+        //inyeccion de dependencias - repository
+        @Autowired
+        Icomentarios dao;
+    
+        @Override
+        public List<comentarios> agregarcomentarios(comentarios comentarios) {
+            return dao.addcomentarios(comentarios);
+        }
+    
+        @Override
+        public comentarios actualizarcomentarios(comentarios comentarios) {
+            //busqueda del id a ver si existe
+            return dao.uppcomentarios(comentarios);
+        }
+    
+        @Override
+        public List<comentarios> todascomentarios() {
+            return dao.getAllcomentarios();
+        }
+    
+        @Override
+        public comentarios buscarIdcomentarios(int id) {
+            return dao.getIdcomentarios(id);
+        }
+
+        @Override
+        public boolean bajacomentarios(int id) {
+            // TODO Auto-generated method stub
+            return false;
+        }
+    
+    }
