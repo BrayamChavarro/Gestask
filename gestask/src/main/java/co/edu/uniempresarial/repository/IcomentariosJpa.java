479d2cb8dc6c40913ab1d6a075626cb7b17d0820
Brayam <102553162+Brazamm@users.noreply.github.com>
2024-12-09 21:23:10 -0500

    Create IcomentariosJpa.java

---
+++ gestask/src/main/java/co/edu/uniempresarial/repository/IcomentariosJpa.java
@@ -0,0 +1,10 @@
+package co.edu.uniempresarial.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import co.edu.uniempresarial.model.comentarios;
+
+
+public interface IcomentariosJpa extends JpaRepository<comentarios,Integer> {
+
+}
