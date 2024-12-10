a57a71cdacf912bf9e594ca62b2574d988df89c4
Brayam <102553162+Brazamm@users.noreply.github.com>
2024-12-09 21:25:36 -0500

    Update IcomentariosService.java

--- gestask/src/main/java/co/edu/uniempresarial/service/IcomentariosService.java
+++ gestask/src/main/java/co/edu/uniempresarial/service/IcomentariosService.java
@@ -1,1 +1,16 @@
+package co.edu.uniempresarial.service;
+import java.util.List;
+
+import co.edu.uniempresarial.model.comentarios;
+public interface IcomentariosService {
+    List<comentarios> agregarcomentarios(comentarios comentarios );
+	comentarios actualizarcomentarios (comentarios comentarios);
+	
+	List<comentarios> todascomentarios();
+	comentarios buscarIdcomentarios(int id);
+	
+	
+	boolean bajacomentarios(int id);
+
+}
 
